4dc79bad5ba10b85588ef548a57c6fda75f5ff79
Gerardo <gerardooscarjt@gmail.com>
2016-04-14 09:51:01 +0200

    Add font mimetypes

--- src/reproxy/files/utils.go
+++ src/reproxy/files/utils.go
@@ -11,15 +11,28 @@ import (
 )
 
 var contenttypes = map[string]string{
+
+	// Web
 	".css":  "text/css; charset=utf-8",
-	".js":   "text/javascript; charset=UTF-8",
-	".html": "text/html; charset=utf-8",
 	".htm":  "text/html; charset=utf-8",
-	".ico":  "image/x-icon",
+	".html": "text/html; charset=utf-8",
+	".js":   "text/javascript; charset=UTF-8",
 	".json": "application/json; charset=UTF-8",
-	".png":  "image/png",
+
+	// Images
+	".ico":  "image/x-icon",
 	".jpg":  "image/jpeg",
 	".jpeg": "image/jpeg",
+	".png":  "image/png",
+	".svg":  "image/svg+xml",
+
+	// Fonts:
+	".eot":   "application/vnd.ms-fontobject",
+	".otf":   "application/x-font-opentype",
+	".sfnt":  "application/font-sfnt",
+	".ttf":   "application/x-font-ttf",
+	".woff":  "application/font-woff",
+	".woff2": "application/font-woff2",
 }
 
 func addcontenttype(c *golax.Context, filename string) {
